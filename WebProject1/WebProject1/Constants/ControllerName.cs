e753c40a870d91666996fdd05e5a242890561bb1
JBihms <keisanki.kyokudo@gmail.com>
2015-05-19 09:25:50 -0700

    ASP .NET MVC bp sample

---
+++ WebProject1/WebProject1/Constants/ControllerName.cs
@@ -0,0 +1,9 @@
+﻿namespace WebProject1.Constants
+{
+    public static class ControllerName
+    {
+        public const string Elmah = "Elmah";
+        public const string Error = "Error";
+        public const string Home = "Home";
+    }
+}
